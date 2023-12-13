98519e81d7e7f9ad9a514fa30d30f7924a8fb7e6
임찬섭 <lcs02314@naver.com>
2023-12-13 21:12:31 +0900

    각각 DAO 생성
    service를 사용하도록 바꿨고 doa 변수를 선언해서 초기화 시켰다

--- Airport/src/main/java/Controller.java
+++ Airport/src/main/java/Controller.java
@@ -1,41 +1,39 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet("/Controller")
+@WebServlet("/controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	FlightDAO f_dao;
+	ReservationDAO r_dao;
+	UserDAO u_dao;
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Controller() {
-        super();
-        // TODO Auto-generated constructor stub
+	public void init(ServletConfig config) throws ServletException{
+        super.init(config);
+        f_dao = new FlightDAO();
+    	r_dao = new ReservationDAO();
+    	u_dao = new UserDAO() ;
     }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String action = request.getParameter("action");
+		f_dao = new FlightDAO();
+    	r_dao = new ReservationDAO();
+    	u_dao = new UserDAO() ;
 	}
 
 }
