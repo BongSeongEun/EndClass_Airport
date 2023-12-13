812d600539216c404aa4acad64cf13e19a3a606b
SeongEun <bongfam2@naver.com>
2023-12-14 04:54:55 +0900

    DO 클래스 업데이트

--- Airport/src/main/java/Flight.java
+++ Airport/src/main/java/Flight.java
@@ -1,65 +1,64 @@
 public class Flight {
-    private int ticketId;
-    
-    private String airlineId;
-    private String source;
-    private String destination;
-    private String time;
-    private String seat;
-    private double price;
-    
-    
-	public int getTicketId() {
-		return ticketId;
+	private int airplaneId;
+	private String airline;
+	private String source;
+	private String destination;
+	private String time;
+	private String seat;
+	private double price;
+
+	public int getAirplaneId() {
+		return airplaneId;
 	}
-	public void setTicketId(int flightId) {
-		this.ticketId = flightId;
+
+	public void setAirplaneId(int airplaneId) {
+		this.airplaneId = airplaneId;
 	}
-	
-	
-	public String getAirlineId() {
-		return airlineId;
+
+	public String getAirline() {
+		return airline;
 	}
-	public void setAirlineId(String airlineId) {
-		this.airlineId = airlineId;
+
+	public void setAirline(String airline) {
+		this.airline = airline;
 	}
-	
-	
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
-	
-	
+
 	public String getDestination() {
 		return destination;
 	}
+
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
-	
-	
+
 	public String getTime() {
 		return time;
 	}
+
 	public void setTime(String time) {
 		this.time = time;
 	}
-	
-	
+
 	public String getSeat() {
 		return seat;
 	}
+
 	public void setSeat(String seat) {
 		this.seat = seat;
 	}
-	
-	
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
